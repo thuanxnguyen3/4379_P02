2d9bebeb94acb43cee5487d8addc037c0f2b7d88
Peter <pedroramoshernandez01@gmail.com>
2023-04-12 03:00:21 -0500

    Changed script for camara
    Added code for camara movement

--- Assets/AccelerometerMove.cs
+++ Assets/AccelerometerMove.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class AccelerometerMove : MonoBehaviour
 {
 
-    //public bool isFlat = true;
-    //private Rigidbody rigidBody;
+    public bool isFlat = true;
+    private Rigidbody rigidBody;
 
     private void Start()
     {
-        //rigidBody = GetComponent<Rigidbody>();
+        rigidBody = GetComponent<Rigidbody>();
     }
     private void Update()
     {
-        /* Vector3 tilt = Input.acceleration;
+         Vector3 tilt = Input.acceleration;
 
         if (isFlat)
             tilt = Quaternion.Euler(90, 0, 0) * tilt; // rotate a vector super quick
 
         rigidBody.AddForce(tilt);
 
-        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan); */
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan); 
 
 
 
--- Assets/CamaraController.cs
+++ Assets/CamaraController.cs
@@ -4,7 +4,34 @@ using UnityEngine;
 
 public class CamaraController : MonoBehaviour
 {
-    
+
+    public GameObject player;
+
+    private Vector3 playerOffset;
+
+    // Use this for initialization
+
+    void Start()
+
+    {
+
+        playerOffset = transform.position - player.transform.position; // distance from camera
+
+    }
+
+
+
+    // Update is called once per frame
+
+    void Update()
+
+    {
+
+        transform.LookAt(player.transform);
+
+        transform.position = player.transform.position + playerOffset;
+
+    }
 
 
 }
